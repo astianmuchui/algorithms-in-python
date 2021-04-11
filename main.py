de831a5c2716ad3e9e8699dbce3f44ce2f54e726
astian4 <sebastianmuchui79@gmail.com>
2021-04-11 19:44:18 +0300

    Get mean of numbers

---
+++ main.py
@@ -0,0 +1,18 @@
+    m = 10
+    y = 15 
+    sum = m+y
+    print(sum)
+    #get mean
+    math = 80
+    eng = 70
+    swahili = 60
+    chem = 80
+    phy = 70
+    geography = 50
+    aviation = 60
+    bio = 90
+    total = math+eng+swahili+chem+phy+geography+aviation+bio
+    mean = total/8
+    print(mean)
+
+
